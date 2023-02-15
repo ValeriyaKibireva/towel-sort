b0d5aba1863c3458d570c4b46f6279631ac27126
ValeriyaKibireva <kiborglera@gmail.com>
2023-02-15 16:38:42 +0100

    feat: completed task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix = []) {
+
+    return matrix.map((el, i) => (i + 1) % 2 ? el : el.reverse()).flat();
+    console.log(matrix)
+
 }
